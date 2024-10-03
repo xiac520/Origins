e32f5b26fe4982534a358fe22e41a332ae9de096
xiac520 <love@xiac.cc>
2024-10-03 13:31:19 +0800

    更新

--- public/api.js
+++ public/api.js
@@ -1,7 +1,5 @@
-const API_URL = 'https://origins.dnscron.com';
-
 async function fetchAPI(url, charset, callback, raw) {
-  let proxyUrl = `${API_URL}/${raw ? 'raw' : 'get'}?url=${encodeURIComponent(url)}`;
+  let proxyUrl = `/api/${raw ? 'raw' : 'get'}?url=${encodeURIComponent(url)}`;
   if (charset) {
     proxyUrl += `&charset=${encodeURIComponent(charset)}`;
   }
